4f6666ea494fbe0afcbeac2e8beb3bb09344bb2c
Minh Nguyen <50205705+ABCbum@users.noreply.github.com>
2025-08-20 12:57:26 +0700

    feat: support macos for browser script

--- baselines/naviqate/browser.sh
+++ baselines/naviqate/browser.sh
@@ -4,15 +4,23 @@ PORT=9222
 WINDOW_SIZE="1280,720"
 PROFILE_DIR="/tmp/chrome-profile"
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    CHROME_CMD="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+else
+    CHROME_CMD="google-chrome"
+fi
+
 # Kill any existing Chrome instance using that port
 pkill -f "google-chrome.*--remote-debugging-port=${PORT}" || true
+pkill -f "Google Chrome.*--remote-debugging-port=${PORT}" || true
 
 # Make sure profile dir exists
 mkdir -p "$PROFILE_DIR"
 
 # Start Chrome
 echo "Starting local Chrome..."
-google-chrome \
+"$CHROME_CMD" \
     --remote-debugging-port=${PORT} \
     --remote-debugging-address=127.0.0.1 \
     --user-data-dir=${PROFILE_DIR} \
