9ba9b54e51ff96cd5fac0ef879806e1987c9c27b
Minh Nguyen <50205705+ABCbum@users.noreply.github.com>
2025-10-04 22:24:28 +0800

    refactor: bookstack support deterministic starting page

--- testcases/bookstack/chapter.py
+++ testcases/bookstack/chapter.py
@@ -1,15 +1,20 @@
 from bookstack.conftest import BookStackTestData, create_chapter
+from bookstack.utilities import (
+    navigate_to_book,
+    navigate_to_chapter,
+    navigate_back_to_book_from_chapter,
+)
 from playwright.sync_api import Page
 from tracing_api import insert_start_event_to_page
 from tracing_api import traced_expect as expect
 
 
-def test_create_chapter(created_book_page: Page, test_data: BookStackTestData) -> None:
-    # Check content of the chapter page
-    insert_start_event_to_page(created_book_page)
+def test_create_chapter(logged_in_page: Page, test_data: BookStackTestData) -> None:
+    insert_start_event_to_page(logged_in_page)
+    navigate_to_book(logged_in_page, test_data.book_name)
 
     created_chapter_page = create_chapter(
-        created_book_page, test_data.chapter_name, test_data.chapter_description
+        logged_in_page, test_data.chapter_name, test_data.chapter_description
     )
 
     expect(created_chapter_page.locator("h1")).to_contain_text(test_data.chapter_name)
@@ -26,88 +31,70 @@ def test_create_chapter(created_book_page: Page, test_data: BookStackTestData) -
     )
 
     # Navigate back to book page and check
-    created_chapter_page.get_by_label("Breadcrumb").get_by_role(
-        "link", name=test_data.book_name
-    ).click()
+    navigate_back_to_book_from_chapter(created_chapter_page, test_data.book_name)
     expect(created_chapter_page.get_by_role("main")).to_contain_text(
         test_data.book_name
     )
 
 
-def test_read_chapter(created_chapter_page: Page, test_data: BookStackTestData) -> None:
-    # Navigate back to the book, check the chapter
-    insert_start_event_to_page(created_chapter_page)
+def test_read_chapter(logged_in_page: Page, test_data: BookStackTestData) -> None:
+    insert_start_event_to_page(logged_in_page)
+    navigate_to_chapter(logged_in_page, test_data.book_name, test_data.chapter_name)
 
-    created_chapter_page.get_by_label("Breadcrumb").get_by_role(
-        "link", name=test_data.book_name
-    ).click()
-
-    # Click the chapter link and check content
-    expect(created_chapter_page.get_by_role("main")).to_contain_text(
-        test_data.chapter_name
-    )
-    created_chapter_page.get_by_role("link", name=test_data.chapter_name).first.click()
-
-    expect(created_chapter_page.locator("h1")).to_contain_text(test_data.chapter_name)
-    expect(created_chapter_page.get_by_role("main")).to_contain_text(
+    expect(logged_in_page.locator("h1")).to_contain_text(test_data.chapter_name)
+    expect(logged_in_page.get_by_role("main")).to_contain_text(
         test_data.chapter_description
     )
 
 
-def test_update_chapter(
-    created_chapter_page: Page, test_data: BookStackTestData
-) -> None:
-    insert_start_event_to_page(created_chapter_page)
+def test_update_chapter(logged_in_page: Page, test_data: BookStackTestData) -> None:
+    insert_start_event_to_page(logged_in_page)
+    navigate_to_chapter(logged_in_page, test_data.book_name, test_data.chapter_name)
 
-    created_chapter_page.get_by_role("link", name="Edit").click()
+    logged_in_page.get_by_role("link", name="Edit").click()
 
     # Name
-    created_chapter_page.get_by_role("textbox", name="Name").click()
-    created_chapter_page.get_by_role("textbox", name="Name").fill(
+    logged_in_page.get_by_role("textbox", name="Name").click()
+    logged_in_page.get_by_role("textbox", name="Name").fill(
         test_data.chapter_name_updated
     )
 
     # Description
-    created_chapter_page.locator(
-        'iframe[title="Rich Text Area"]'
-    ).content_frame.get_by_text(test_data.chapter_description).click()
-    created_chapter_page.locator(
-        'iframe[title="Rich Text Area"]'
-    ).content_frame.get_by_label("Rich Text Area. Press ALT-0").fill(
-        test_data.chapter_description_updated
-    )
+    logged_in_page.locator('iframe[title="Rich Text Area"]').content_frame.get_by_text(
+        test_data.chapter_description
+    ).click()
+    logged_in_page.locator('iframe[title="Rich Text Area"]').content_frame.get_by_label(
+        "Rich Text Area. Press ALT-0"
+    ).fill(test_data.chapter_description_updated)
 
     # Tags
-    created_chapter_page.get_by_text("Chapter Tags Add some tags to").click()
-    created_chapter_page.locator('input[name="tags[0][value]"]').click()
-    created_chapter_page.locator('input[name="tags[0][value]"]').fill(
-        "Sample Tag Updated"
-    )
+    logged_in_page.get_by_text("Chapter Tags Add some tags to").click()
+    logged_in_page.locator('input[name="tags[0][value]"]').click()
+    logged_in_page.locator('input[name="tags[0][value]"]').fill("Sample Tag Updated")
 
-    created_chapter_page.get_by_role("button", name="Save Chapter").click()
+    logged_in_page.get_by_role("button", name="Save Chapter").click()
 
     # Check content.
     expect(
-        created_chapter_page.get_by_role("alert").filter(
+        logged_in_page.get_by_role("alert").filter(
             has_text="Chapter successfully updated"
         )
     ).to_be_visible(timeout=1000)
-    expect(created_chapter_page.locator("h1")).to_contain_text(
-        test_data.chapter_name_updated
-    )
-    expect(created_chapter_page.get_by_role("main")).to_contain_text(
+    expect(logged_in_page.locator("h1")).to_contain_text(test_data.chapter_name_updated)
+    expect(logged_in_page.get_by_role("main")).to_contain_text(
         test_data.chapter_description_updated
     )
 
 
-def test_delete_chapter(created_chapter_page: Page) -> None:
-    insert_start_event_to_page(created_chapter_page)
+def test_delete_chapter(logged_in_page: Page, test_data: BookStackTestData) -> None:
+    insert_start_event_to_page(logged_in_page)
+    navigate_to_chapter(logged_in_page, test_data.book_name, test_data.chapter_name)
 
-    created_chapter_page.get_by_role("link", name="Delete").click()
-    created_chapter_page.get_by_role("button", name="Confirm").click()
+    logged_in_page.get_by_role("link", name="Delete").click()
+    logged_in_page.get_by_role("button", name="Confirm").click()
 
     expect(
-        created_chapter_page.get_by_role("alert").filter(
+        logged_in_page.get_by_role("alert").filter(
             has_text="Chapter successfully deleted"
         )
     ).to_be_visible()
